e1bb20a025d978644c1096b9b39541635dda35f0
Dicko Finus <dickofn@gmail.com>
2019-03-26 10:49:40 +0700

    initialize vuetify and added main layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,61 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-app id="inspire">
+      <v-navigation-drawer fixed v-model="drawer" app>
+        <v-list dense>
+          <v-list-tile @click>
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click>
+            <v-list-tile-action>
+              <v-icon>contact_mail</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Contact</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar color="indigo" dark fixed app>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Application</v-toolbar-title>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout justify-center align-center>
+            <v-flex text-xs-center>
+              <v-tooltip left>
+                <v-btn icon large :href="source" target="_blank" slot="activator">
+                  <v-icon large>code</v-icon>
+                </v-btn>
+                <span>Source</span>
+              </v-tooltip>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+      <v-footer color="indigo" app inset>
+        <span class="white--text">&copy; 2017</span>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: "App",
+  data () {
+    return {
+      drawer: null
     }
+  },
+  props: {
+    source: String
   }
 }
-</style>
+</script>
