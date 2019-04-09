ad57e0d2dedc7921ab12f86fea743c9914558e10
Dicko Finus <dickofn@gmail.com>
2019-04-09 15:08:52 +0700

    import axios in main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios' 
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -12,6 +15,7 @@ import SmartTable from 'vuejs-smart-table'
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
+Vue.use(VueAxios, axios)
 Vue.use(SmartTable)
 
 new Vue({
