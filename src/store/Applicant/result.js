c8453d1cb7aa15eae497ae3d0014c45b6c18df9f
Dicko Finus <dickofn@gmail.com>
2019-04-10 17:32:11 +0700

    result store

--- src/store/Applicant/result.js
+++ src/store/Applicant/result.js
@@ -1,10 +1,30 @@
-// import Axios from "axios";
+import Axios from "axios";
 
 export default {
   state: {
+    result: null
   },
   mutations: {
+    UPDATE_RESULT(state, payload) {
+      state.result = payload;
+    }
   },
   actions: {
+    getResult({ commit }, applicantId) {
+      commit("UPDATE_LOADING", true);
+      Axios.get(process.env.VUE_APP_API_URL + "/exam/result/get/" + applicantId)
+        .then(res => {
+          commit("UPDATE_RESULT", res.data.data);
+          commit("UPDATE_LOADING", false);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    clearResult({ commit }) {
+      commit("UPDATE_LOADING", true);
+      commit("UPDATE_RESULT", null);
+      commit("UPDATE_LOADING", false);
+    }
   }
 };
