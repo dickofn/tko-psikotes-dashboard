0685962168b1873babe9b29a69f8a1e3cbe79847
Dicko Finus <dickofn@gmail.com>
2019-04-23 17:05:02 +0700

    auth protection in router

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ import Report from "./views/Applicant/Result/Report.vue";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
@@ -51,3 +51,19 @@ export default new Router({
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login', '/'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    return next('/login');
+  }
+
+  next();
+})
+
+export default router
+
