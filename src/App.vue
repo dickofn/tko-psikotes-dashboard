8ee38b91172ae87ce95ccbde66e0174a8931ed09
Dicko Finus <dickofn@gmail.com>
2019-04-25 15:17:25 +0700

    session protection

--- src/App.vue
+++ src/App.vue
@@ -66,14 +66,7 @@ export default {
     }
   },
   created () {
-    this.$http.interceptors.response.use(undefined, err => {
-      return new Promise(() => {
-        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
-          this.$store.dispatch('logout')
-        }
-        throw err;
-      });
-    });
+    
   }
 }
 </script>
