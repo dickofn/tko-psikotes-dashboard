5b6cc1a7123f5b8e4ebbc8303a7b6e1a6f92aa8e
Dicko Finus <dickofn@gmail.com>
2019-04-26 16:02:07 +0700

    change to promise style

--- src/store/Applicant/result.js
+++ src/store/Applicant/result.js
@@ -15,15 +15,21 @@ export default {
   },
   actions: {
     getResult({ commit }, applicantId) {
-      commit("UPDATE_LOADING", true);
-      Axios.get(process.env.VUE_APP_API_URL + "/exam/result/get/" + applicantId, auth)
-        .then(res => {
-          commit("UPDATE_RESULT", res.data.data);
-          commit("UPDATE_LOADING", false);
-        })
-        .catch(e => {
-          console.log(e);
-        });
+      return new Promise((resolve, reject) => {
+        commit("UPDATE_LOADING", true);
+        Axios.get(
+          process.env.VUE_APP_API_URL + "/exam/result/get/" + applicantId,
+          auth
+        )
+          .then(res => {
+            commit("UPDATE_RESULT", res.data.data);
+            commit("UPDATE_LOADING", false);
+            resolve(res);
+          })
+          .catch(e => {
+            reject(e);
+          });
+      });
     },
     clearResult({ commit }) {
       commit("UPDATE_LOADING", true);
