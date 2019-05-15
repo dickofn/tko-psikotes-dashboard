c08fb3e0feed69f3ac1719afd237af9abe6f9359
Dicko Finus <dickofn@gmail.com>
2019-05-15 14:39:35 +0700

    psychotest time setting

---
+++ src/components/Setting/Psychotest/TimeDialog.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card>
+    <v-card-title primary-title>
+      <h1 class="headline">{{ $t('pengaturanWaktuPsikotes') }} - {{ title }}</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form ref="form" v-model="valid" @submit.prevent="submit">
+        <v-container grid-list-xs>
+          <v-layout row wrap>
+            <v-flex xs12>
+              <v-text-field name="value" :label="$t('waktu')+' ('+$t('milidetik')+')'" v-model="value" type="number" :rules="[rules.required]"></v-text-field>
+            </v-flex>
+            <v-flex xs12 text-xs-right>
+              <v-btn type="submit" :disabled="!valid" color="red darken-2" :dark="valid">Submit</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['title', 'examId'],
+  data () {
+    return {
+      valid: false,
+      value: null,
+      rules: {
+        required: v => !!v || this.$t('wajibDiisi')+"!",
+      }
+    }
+  },
+  methods: {
+    submit () {
+      console.log(this.value);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/Setting/Psychotest/Time.vue
+++ src/views/Setting/Psychotest/Time.vue
@@ -6,6 +6,74 @@
           <v-card-title primary-title>
             <h1 class="headline">{{ $t('pengaturanWaktuPsikotes') }}</h1>
           </v-card-title>
+          <v-card-text>
+            <v-container grid-list-xs>
+              <v-layout row wrap>
+                <v-flex xs12 md6 lg3 text-xs-center>
+                  <v-dialog v-model="timeSettingDialogDisc" max-width="700px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn v-on="on" color="red darken-2" block large dark>DISC</v-btn>
+                    </template>
+                    <time-dialog :title="'DISC'" :examId="1"></time-dialog>
+                  </v-dialog>
+                </v-flex>
+                <v-flex xs12 md6 lg3 text-xs-center>
+                  <v-dialog v-model="timeSettingDialogApm" max-width="700px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn v-on="on" color="red darken-2" block large dark>APM</v-btn>
+                    </template>
+                    <time-dialog :title="'APM'" :examId="2"></time-dialog>
+                  </v-dialog>
+                </v-flex>
+                <v-flex xs12 md6 lg3 text-xs-center>
+                  <v-dialog v-model="timeSettingDialogEng" max-width="700px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn
+                        v-on="on"
+                        color="red darken-2"
+                        block
+                        large
+                        dark
+                      >{{ $t('bahasaInggris') }}</v-btn>
+                    </template>
+                    <time-dialog :title="$t('bahasaInggris')" :examId="3"></time-dialog>
+                  </v-dialog>
+                </v-flex>
+                <v-flex xs12 md6 lg3 text-xs-center>
+                  <v-dialog v-model="timeSettingDialogTkd1" max-width="700px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn v-on="on" color="red darken-2" block large dark>TKD-1</v-btn>
+                    </template>
+                    <time-dialog :title="'TKD-1'" :examId="4"></time-dialog>
+                  </v-dialog>
+                </v-flex>
+                <v-flex xs12 md6 lg3 text-xs-center>
+                  <v-dialog v-model="timeSettingDialogTkd5" max-width="700px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn v-on="on" color="red darken-2" block large dark>TKD-5</v-btn>
+                    </template>
+                    <time-dialog :title="'TKD-5'" :examId="5"></time-dialog>
+                  </v-dialog>
+                </v-flex>
+                <v-flex xs12 md6 lg3 text-xs-center>
+                  <v-dialog v-model="timeSettingDialogTkd6" max-width="700px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn v-on="on" color="red darken-2" block large dark>TKD-6</v-btn>
+                    </template>
+                    <time-dialog :title="'TKD-6'" :examId="6"></time-dialog>
+                  </v-dialog>
+                </v-flex>
+                <v-flex xs12 md6 lg3 text-xs-center>
+                  <v-dialog v-model="timeSettingDialogPap" max-width="700px">
+                    <template v-slot:activator="{ on }">
+                      <v-btn v-on="on" color="red darken-2" block large dark>Papikostik</v-btn>
+                    </template>
+                    <time-dialog :title="'Papikostik'" :examId="7"></time-dialog>
+                  </v-dialog>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
@@ -13,8 +81,23 @@
 </template>
 
 <script>
-export default {
+import timeDialog from '@/components/Setting/Psychotest/TimeDialog'
 
+export default {
+  data () {
+    return {
+      timeSettingDialogDisc: false,
+      timeSettingDialogApm: false,
+      timeSettingDialogEng: false,
+      timeSettingDialogTkd1: false,
+      timeSettingDialogTkd5: false,
+      timeSettingDialogTkd6: false,
+      timeSettingDialogPap: false
+    }
+  },
+  components: {
+    timeDialog
+  }
 }
 </script>
 
